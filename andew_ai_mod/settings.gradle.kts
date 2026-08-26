f4c918adde098d2bf7918ebc0569b18515ad1700
serkolinas86-tech <serkolinas86@gmail.com>
2026-08-26 12:32:24 +0700

    Add andew_ai_mod/settings.gradle.kts

---
+++ andew_ai_mod/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOSITORIES)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Andew AI Local"
+include ':app'
