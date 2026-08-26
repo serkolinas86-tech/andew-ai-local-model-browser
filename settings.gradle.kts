05fe1f270252654374642709b0ff3a0219f74d71
serkolinas86-tech <serkolinas86@gmail.com>
2026-08-26 12:51:23 +0700

    Fix settings.gradle.kts - simplified for Gradle 9.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,17 @@
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(org.gradle.api.artifacts.repositories.RepositoriesMode.FAIL_ON_PROJECT_REPOSITORIES)
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositoriesMode.set(org.gradle.api.artifacts.repositories.RepositoriesMode.FAIL_ON_PROJECT_REPOSITORIES)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
 rootProject.name = "Andew AI Local"
